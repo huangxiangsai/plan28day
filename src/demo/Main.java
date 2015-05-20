8621a2444dc815b25a5b00e5e0fce17f5fe12bc1
devsai <469181336@qq.com>
2015-05-20 16:08:07 +0800

    add all

---
+++ src/demo/Main.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Main {
+	
+	public Main() {
+		System.out.print("like mac,hello mac");
+	}
+	
+	public static void main(String[] args) {
+		Main m = new Main();
+	}
+
+}
